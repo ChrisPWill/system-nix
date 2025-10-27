41032148496a113b29cabf1daf785235b5162cb6
Chris Williams <chris@chrispwill.com>
2025-10-27 14:22:34 +1100

    Add basic flake with node20 devShell

---
+++ devShells/node20.nix
@@ -0,0 +1,19 @@
+{pkgs}:
+pkgs.mkShell {
+  # Add build dependencies
+  packages = with pkgs; [
+    nodejs_20
+    (yarn.override {
+      nodejs = nodejs_20;
+    })
+  ];
+
+  env = {
+    # Set npm global packages path
+    npm_config_prefix = "/Users/cwilliams/.node20-packages";
+  };
+
+  # Load custom bash code
+  shellHook = ''
+  '';
+}
---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Simple flake with a devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # https://github.com/numtide/blueprint
+    # Used for managing flake via standard folder structure
+    blueprint.url = "github:numtide/blueprint";
+    blueprint.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  # Load config via blueprint https://github.com/numtide/blueprint
+  outputs = inputs: inputs.blueprint {inherit inputs;};
+}
