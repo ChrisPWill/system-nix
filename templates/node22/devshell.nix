00829ea1499475f9eb64b9539e6b11070c3af3c3
Chris Williams <chris@chrispwill.com>
2025-11-09 21:04:36 +1100

    Try node22 template

---
+++ templates/node22/devshell.nix
@@ -0,0 +1,24 @@
+{pkgs, ...}:
+let inherit (pkgs) stdenv; in
+pkgs.mkShell {
+  # Add build dependencies
+  packages = with pkgs; [
+    nodejs_22
+    (yarn.override {
+      nodejs = nodejs_22;
+    })
+  ];
+
+  env = {
+    # Set npm global packages path
+    npm_config_prefix = "${if stdenv.isDarwin then "/Users" else "/home"}/cwilliams/.node22-packages";
+  };
+
+  # Load custom bash code
+  shellHook = ''
+    # Add node22 packages bin to PATH
+    export PATH="${if stdenv.isDarwin then "/Users" else "/home"}/cwilliams/.node22-packages:$PATH"
+
+    export PS1="(node22) $PS1"
+  '';
+}
