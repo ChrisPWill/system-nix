df732e5f8e7e156acfd75c7ebb687d45e7cac6ce
Chris Williams <chris@chrispwill.com>
2025-11-09 20:42:43 +1100

    Dynamically set paths in devshells

--- devshells/node22.nix
+++ devshells/node22.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}:
+let inherit (pkgs) stdenv; in
 pkgs.mkShell {
   # Add build dependencies
   packages = with pkgs; [
@@ -10,12 +11,12 @@ pkgs.mkShell {
 
   env = {
     # Set npm global packages path
-    npm_config_prefix = "/Users/cwilliams/.node22-packages";
+    npm_config_prefix = "${if stdenv.isDarwin then "/Users" else "/home"}/cwilliams/.node22-packages";
   };
 
   # Load custom bash code
   shellHook = ''
     # Add node22 packages bin to PATH
-    export PATH="/Users/cwilliams/.node22-packages/bin:$PATH";
+    export PATH="${if stdenv.isDarwin then "/Users" else "/home"}/cwilliams/.node22-packages:$PATH";
   '';
 }
