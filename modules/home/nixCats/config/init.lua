ab1e4ee63881cfd58bd6f3f16a1b98194ce095db
Chris Williams <chris@chrispwill.com>
2025-11-10 21:36:43 +1100

    Add format on save (plus commands to disable/enable)

--- modules/home/nixCats/config/init.lua
+++ modules/home/nixCats/config/init.lua
@@ -137,7 +137,7 @@ require("lze").load({
 					},
 				},
 			})
-			require('treesitter-context').setup({})
+			require("treesitter-context").setup({})
 		end,
 	},
 	{
@@ -389,6 +389,14 @@ require("lze").load({
 					typescript = nixCats("node") and { "prettierd", "prettier" } or nil,
 					nix = nixCats("nix") and { "alejandra" } or nil,
 				},
+
+				format_on_save = function(bufnr)
+					-- Disable with a global or buffer-local variable
+					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+						return
+					end
+					return { timeout_ms = 1000, lsp_format = "fallback" }
+				end,
 			})
 
 			vim.keymap.set({ "n", "v" }, "<leader>FF", function()
@@ -399,6 +407,24 @@ require("lze").load({
 					stop_after_first = true,
 				})
 			end, { desc = "[F]ormat [F]ile" })
+
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					-- FormatDisable! will disable formatting just for this buffer
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true,
+			})
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
+			})
 		end,
 	},
 	{
