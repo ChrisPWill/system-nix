df7fdea4f9ea904657f98ee9115ac67538ec7deb
Chris Williams <chris@chrispwill.com>
2025-11-25 00:15:49 +1100

    Set default NixOS shell to zsh

--- modules/nixos/host-shared.nix
+++ modules/nixos/host-shared.nix
@@ -1,11 +1,15 @@
 {pkgs, ...}: {
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  environment.systemPackages =
+  environment.systemPackages = with pkgs;
     [
-      pkgs.btop
+      btop
     ]
     # you can check if host is darwin by using pkgs.stdenv.isDarwin
     #++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [pkgs.xbar])
     ;
+
+  programs.zsh.enable = true;
+
+  users.defaultUserShell = pkgs.zsh;
 }
