c1c50da032fcb6b7b048fe57bfb1177e1fa1a96b
Chris Williams <cwilliams@atlassian.com>
2025-11-25 14:48:04 +1100

    Improve stateVersion comment

--- hosts/cwilliams-work-laptop/darwin-configuration.nix
+++ hosts/cwilliams-work-laptop/darwin-configuration.nix
@@ -55,5 +55,13 @@
   users.users.cwilliams.home = /Users/cwilliams;
   system.primaryUser = "cwilliams";
 
-  system.stateVersion = 4;
+  # Every once in a while, a new nix-darwin release may change configuration defaults
+  # in a way incompatible with stateful data. For instance, if the default version of
+  # PostgreSQL changes, the new version will probably be unable to read your existing
+  # databases. To prevent such breakage, you can set the value of this option to the
+  # nix-darwin release with which you want to be compatible. The effect is that
+  # nix-darwin will option defaults corresponding to the specified release (such as
+  # using an older version of PostgreSQL).
+  # It should be fine to leave this.
+  system.stateVersion = 4; # did you read the comment?
 }
