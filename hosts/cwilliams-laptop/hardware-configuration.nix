a25ed85639e868aac9078a27524ac6785eac5151
Chris Williams <chris@chrispwill.com>
2025-12-09 23:10:06 +1100

    Enable graphics drivers

--- hosts/cwilliams-laptop/hardware-configuration.nix
+++ hosts/cwilliams-laptop/hardware-configuration.nix
@@ -1,31 +1,34 @@
-{ config, lib, pkgs, modulesPath, ... }:
-
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  config,
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-intel"];
+  boot.extraModulePackages = [];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/d91a6e87-a656-4cd1-8eae-5321fad4f5a8";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/d91a6e87-a656-4cd1-8eae-5321fad4f5a8";
+    fsType = "ext4";
+  };
 
   boot.initrd.luks.devices."luks-3064df00-c86d-44b5-8aa1-3c9dacf3cbbf".device = "/dev/disk/by-uuid/3064df00-c86d-44b5-8aa1-3c9dacf3cbbf";
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/EFEC-F0BD";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/EFEC-F0BD";
+    fsType = "vfat";
+    options = ["fmask=0077" "dmask=0077"];
+  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/045e3a12-29ba-4b4e-af9c-cdd7c52d8eca"; }
-    ];
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/045e3a12-29ba-4b4e-af9c-cdd7c52d8eca";}
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -37,4 +40,15 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Graphics
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia.open = true;
+  hardware.nvidia.prime = {
+    sync.enable = true;
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+    #amdgpuBusId = "PCI:54:0:0"; # If you have an AMD iGPU
+  };
 }
