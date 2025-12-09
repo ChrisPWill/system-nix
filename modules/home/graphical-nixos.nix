40924173a16a8f7969831d91f91201fb531d5005
Chris Williams <chris@chrispwill.com>
2025-12-09 19:52:16 +1100

    Move niri config to repo

--- modules/home/graphical-nixos.nix
+++ modules/home/graphical-nixos.nix
@@ -1,4 +1,10 @@
-{inputs, ...}: {...}: {
+{
+  inputs,
+  config,
+  ...
+}: let
+  niriDir = "${config.homeModuleDir}/niri";
+in {
   imports = [
     inputs.dankMaterialShell.homeModules.dankMaterialShell.default
     inputs.dankMaterialShell.homeModules.dankMaterialShell.niri
@@ -14,5 +20,12 @@
       enableKeybinds = true;
       enableSpawn = true;
     };
+
+    programs.niri.config = null;
+    xdg.configFile."niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/config.kdl";
+    xdg.configFile."niri/dms/alttab.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/alttab.kdl";
+    xdg.configFile."niri/dms/colors.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/alttab.kdl";
+    xdg.configFile."niri/dms/layout.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/alttab.kdl";
+    xdg.configFile."niri/dms/wpblur.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/alttab.kdl";
   };
 }
