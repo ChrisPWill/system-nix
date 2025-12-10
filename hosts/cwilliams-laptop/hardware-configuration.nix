93c71a37ba1139864d38aae1030e573f66e05906
Chris Williams <chris@chrispwill.com>
2025-12-10 19:55:57 +0900

    Prime offloading updates

--- hosts/cwilliams-laptop/hardware-configuration.nix
+++ hosts/cwilliams-laptop/hardware-configuration.nix
@@ -47,7 +47,9 @@
   hardware.nvidia.open = true;
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.prime = {
-    sync.enable = true;
+    offload.enable = true;
+    offload.enableOffloadCmd = true;
+
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
     #amdgpuBusId = "PCI:54:0:0"; # If you have an AMD iGPU
