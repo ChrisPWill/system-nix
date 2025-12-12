b702fcdf93376a4fb5f49d1b4861a905914731fb
Chris Williams <chris@chrispwill.com>
2025-12-12 17:30:12 +0900

    Add host-specific niri config

--- hosts/cwilliams-laptop/users/cwilliams.nix
+++ hosts/cwilliams-laptop/users/cwilliams.nix
@@ -1,7 +1,15 @@
-{inputs, ...}: {
+{
+  inputs,
+  config,
+  ...
+}: let
+  hostHomeDir = "${config.nixConfigDir}/hosts/cwilliams-laptop/users";
+in {
   imports = [
     inputs.self.homeModules.home-shared
     inputs.self.homeModules.graphical-nixos
     inputs.self.homeModules.personal-machine
   ];
+
+  xdg.configFile."niri/niri-host.kdl".source = config.lib.file.mkOutOfStoreSymlink "${hostHomeDir}/graphical-nixos/niri-host.kdl";
 }
