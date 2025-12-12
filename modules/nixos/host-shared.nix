1ee18793a4113d05d5107ddfbd1dc5754305e18c
Chris Williams <chris@chrispwill.com>
2025-12-12 20:58:56 +0900

    Add some nix caches

--- modules/nixos/host-shared.nix
+++ modules/nixos/host-shared.nix
@@ -1,9 +1,19 @@
 {pkgs, ...}: {
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    substituters = [
+      "https://niri.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+    ];
+  };
   nix.optimise.automatic = true;
 
-  environment.systemPackages = with pkgs;
-    [
+  environment.systemPackages =
+    with pkgs; [
       btop
     ]
     # you can check if host is darwin by using pkgs.stdenv.isDarwin
