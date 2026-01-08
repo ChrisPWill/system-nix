c7ab8fe880a83529358324129a157ea2bc15ec12
Chris Williams <chris@chrispwill.com>
2026-01-08 16:39:41 +0900

    Try fixing niri config by removing DMS option

--- modules/home/graphical-nixos/niri/default.nix
+++ modules/home/graphical-nixos/niri/default.nix
@@ -14,14 +14,16 @@ in {
     programs.dank-material-shell.niri = {
       enableKeybinds = true;
       enableSpawn = true;
+      # https://github.com/AvengeMedia/DankMaterialShell/pull/1239/files
+      includes.enable = false;
     };
 
-    programs.niri.config = null;
     programs.niri.package = pkgs.niri;
     stylix.targets.niri.enable = false;
 
     xdg.portal.xdgOpenUsePortal = true;
 
+    programs.niri.config = null;
     xdg.configFile."niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/config.kdl";
     xdg.configFile."niri/dms-overrides.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/dms-overrides.kdl";
     xdg.configFile."niri/dms/alttab.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/dms/alttab.kdl";
