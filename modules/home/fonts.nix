8b5b0c25b5233df5f077affbf0dbe1b13536302b
Chris Williams <chris@chrispwill.com>
2026-03-14 19:28:45 +0900

    Add Roboto font

--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -4,16 +4,21 @@
     nerd-fonts.fantasque-sans-mono
 
     noto-fonts-cjk-sans
+
+    roboto
+
+    roboto-serif
   ];
 
   fonts.fontconfig.enable = true;
 
   fonts.fontconfig.defaultFonts = {
     sansSerif = [
+      "Roboto"
       "Noto Sans"
       "Noto Sans CJK KR"
     ];
-    serif = ["Noto Serif" "Noto Serif CJK KR"];
+    serif = ["Roboto Serif" "Noto Serif" "Noto Serif CJK KR"];
     monospace = ["FantasqueSansM Nerd Font Mono"];
   };
 }
