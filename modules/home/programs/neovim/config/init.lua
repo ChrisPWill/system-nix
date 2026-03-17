b40dbb5de98a3261999acc659bf8ab50ac38daf3
Chris Williams <chris@chrispwill.com>
2026-03-17 14:30:40 +0900

    Add unittest snippets

--- modules/home/programs/neovim/config/init.lua
+++ modules/home/programs/neovim/config/init.lua
@@ -778,6 +778,15 @@ require("lze").load({
 			require("scissors").setup({
 				snippetDir = (nixCats.configDir or "") .. "/snippets",
 			})
+
+			vim.keymap.set("n", "<leader>xe", function()
+				require("scissors").editSnippet()
+			end, { desc = "Snippet: Edit" })
+
+			-- when used in visual mode, prefills the selection as snippet body
+			vim.keymap.set({ "n", "x" }, "<leader>xa", function()
+				require("scissors").addNewSnippet()
+			end, { desc = "Snippet: Add" })
 		end,
 	},
 	{
