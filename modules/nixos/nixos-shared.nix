02432742520e200c69f080554f903d436e5d6e7e
Chris Williams <chris@chrispwill.com>
2026-03-20 15:36:47 +0900

    Enable ntsync globally for game perf

--- modules/nixos/nixos-shared.nix
+++ modules/nixos/nixos-shared.nix
@@ -8,6 +8,8 @@
   ];
 
   config = {
+    boot.kernelModules = ["ntsync"];
+
     nix.gc.dates = "weekly";
     stylix.enable = true;
     stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
