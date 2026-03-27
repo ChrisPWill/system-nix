f0e820df86cf2aad85acfa7e8443fb0a733866fa
Chris Williams <chris@chrispwill.com>
2026-03-27 17:06:40 +0900

    Modularise audio config

--- modules/nixos/graphical-environment/default.nix
+++ modules/nixos/graphical-environment/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./dank.nix
     ./niri.nix
+    ../audio.nix
   ];
 
   config = {
@@ -36,17 +37,5 @@
 
     services.dbus.enable = true;
     services.upower.enable = true;
-
-    # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-    };
   };
 }
