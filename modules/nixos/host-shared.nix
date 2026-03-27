e7dcd17f134d5cd662b6a9ff729b28664e6d9d82
Chris Williams <chris@chrispwill.com>
2026-03-27 16:53:42 +0900

    Modularise core Nix settings

--- modules/nixos/host-shared.nix
+++ modules/nixos/host-shared.nix
@@ -1,25 +1,7 @@
-{pkgs, ...}: {
-  nix.settings = {
-    fsync-metadata = false;
-    http2 = true;
-
-    experimental-features = ["nix-command" "flakes"];
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-      "https://niri.cachix.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
-    ];
-  };
-  nix.optimise.automatic = true;
-  nix.gc = {
-    automatic = true;
-    options = "--delete-older-than 30d";
-  };
+{pkgs, inputs, ...}: {
+  imports = [
+    inputs.self.nixosModules.nix-core
+  ];
 
   environment.systemPackages =
     with pkgs; [
--- modules/nixos/nixos-shared.nix
+++ modules/nixos/nixos-shared.nix
@@ -8,7 +8,6 @@
   ];
 
   config = {
-    nix.gc.dates = "weekly";
     stylix.enable = true;
     stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
 
