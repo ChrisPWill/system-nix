6fb02ca17d753188103b1646ba68c6c554613ce2
Chris Williams <chris@chrispwill.com>
2026-03-27 15:37:25 +0900

    Add CachyOS Binary Cache

--- modules/nixos/nixos-shared.nix
+++ modules/nixos/nixos-shared.nix
@@ -8,6 +8,9 @@
   ];
 
   config = {
+    # For CachyOS
+    nix.settings.substituters = ["https://cache.garnix.io" "https://attic.xuyh0120.win/lantian"];
+    nix.settings.trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="];
     nixpkgs.overlays = [
       inputs.nix-cachyos-kernel.overlays.default
     ];
