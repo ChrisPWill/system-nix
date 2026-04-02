9cc351501dfdb4453a10a793de06f9d5c71d280d
Chris Williams <chris@chrispwill.com>
2026-04-02 20:45:49 +0900

    Add formatting check

---
+++ checks/formatting.nix
@@ -0,0 +1,17 @@
+{
+  inputs,
+  pkgs,
+  ...
+}:
+(inputs.treefmt-nix.lib.evalModule pkgs {
+  projectRootFile = "flake.nix";
+
+  programs.alejandra.enable = true; # nix
+  programs.prettier.enable = true; # ts, tsx, json, md, scss, css, html, glsl
+  programs.stylua.enable = true; # lua
+  programs.shfmt.enable = true; # shell
+  programs.taplo.enable = true; # toml
+  programs.rustfmt.enable = true; # rust
+  programs.clang-format.enable = true; # cpp, h
+}).config.build.check
+inputs.self
