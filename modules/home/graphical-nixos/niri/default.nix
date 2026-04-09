0ed0560a274c361265d71277fd8e23e81ed24fae
Chris Williams <chris@chrispwill.com>
2026-04-09 12:35:30 +0900

    Just symlink whole dms directory for niri

--- modules/home/graphical-nixos/niri/default.nix
+++ modules/home/graphical-nixos/niri/default.nix
@@ -25,11 +25,7 @@ in {
 
     programs.niri.config = null;
     xdg.configFile."niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/config.kdl";
+    xdg.configFile."niri/dms".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/dms";
     xdg.configFile."niri/dms-overrides.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/dms-overrides.kdl";
-    xdg.configFile."niri/dms/alttab.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/dms/alttab.kdl";
-    xdg.configFile."niri/dms/binds.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/dms/binds.kdl";
-    xdg.configFile."niri/dms/colors.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/dms/colors.kdl";
-    xdg.configFile."niri/dms/layout.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/dms/layout.kdl";
-    xdg.configFile."niri/dms/wpblur.kdl".source = config.lib.file.mkOutOfStoreSymlink "${niriDir}/dms/wpblur.kdl";
   };
 }
