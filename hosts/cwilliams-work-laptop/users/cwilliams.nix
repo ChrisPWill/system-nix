71ce992a6fc826f4780d348c3729f99577e39e80
Chris Williams <chris.williams@sequencehq.com>
2026-08-06 09:24:22 +0100

    Add 1password SSH agent to work laptop

--- hosts/cwilliams-work-laptop/users/cwilliams.nix
+++ hosts/cwilliams-work-laptop/users/cwilliams.nix
@@ -5,4 +5,7 @@
   ];
 
   usesDeterminateNix = true;
+
+  # Use the 1Password SSH agent for keys managed by 1Password.
+  programs.ssh.settings."*".IdentityAgent = "~/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
 }
